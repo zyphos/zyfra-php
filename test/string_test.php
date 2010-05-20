3e57d4f95a417d15978b21d2c59faf55c469eb05
zyphos <zyphos@zyphos.be>
2010-05-20 11:24:41 +0200

    [IMP] string now support object too

--- test/string_test.php
+++ test/string_test.php
@@ -13,4 +13,22 @@ zyfra_debug::printr('$a = ');
 zyfra_debug::printr($a);
 zyfra_debug::printr('$b = ');
 zyfra_debug::printr($b);
+class C{
+    var $c1 = '  Center  ';
+    var $c2 = 'left    ';
+    var $c3 = '    right';
+}
+$c = new C;
+zyfra_debug::printr('Before:');
+zyfra_debug::printr('$c =');
+zyfra_debug::printr($c);
+zyfra_debug::printr('==========');
+zyfra_debug::printr('After:');
+zyfra_debug::printr('$d = zyfra_string::trim($c);');
+$d = zyfra_string::trim($c);
+zyfra_debug::printr('$c = ');
+zyfra_debug::printr($c);
+zyfra_debug::printr('$d = ');
+zyfra_debug::printr($d);
+
 ?>
