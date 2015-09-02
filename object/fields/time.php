4aafa2a64841b6579d3faa32b4e51b6995611b19
Nicolas DS <zyphos@zyphos.be>
2015-09-02 11:11:17 +0200

    [IMP] orm: Add Date Field

--- object/fields/time.php
+++ object/fields/time.php
@@ -12,4 +12,19 @@ class DatetimeField extends Field{
         return 'DATETIME';
     }
 }
+
+class DateField extends Field{
+    var $widget = 'date';
+    
+    function sql_format($value){
+        if (is_int($value)){
+            return "'".gmdate('Y-m-d', $value)."'";
+        }
+        return "'".$value."'";
+    }
+    
+    function get_sql_def(){
+        return 'DATE';
+    }
+}
 ?>
