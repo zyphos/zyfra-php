810e73cde89fbb3879ae16cecc7c8aa1b5561f54
Nicolas DS <zyphos@zyphos.be>
2015-09-21 16:06:18 +0200

    [IMP] ORM: active_record, allow recursive browse in Many2One field

--- object/active_record.php
+++ object/active_record.php
@@ -11,15 +11,21 @@ class ActiveRecord{
     public function __construct($object, $params = array(), $context = array()){
         /* $params is an array:
          * it contains predefined attribute. Ie: $params = array('uid'=>4,'name'=>'test');
+         * or the key id
+         * $param = 8;
          */
         $this->__object = $object;
         $this->__context = $context;
         $this->__data = null;
+        if (!is_array($params)){
+            $params = array($object->_key=>$params);
+        }
         $this->__params = $params;
         $this->__modified_columns = array();
     }
     
     private function __get_data(){
+        // TODO: prefetch all stored field (like Many2one)
         $obj = $this->__object;
         $key = $obj->_key;
         if(!is_null($this->__data) || (!array_key_exists($key, $this->__params) && !array_key_exists('mql_where', $this->__context))) return;
@@ -31,6 +37,7 @@ class ActiveRecord{
     }
     
     private function __add_data($mql_fields){
+        // TODO: Handle One2Many, Many2Many
         if(is_null($this->__data)) return;
         $obj = $this->__object;
         $result = $obj->select($mql_fields.' WHERE '.$this->__mql_where, $this->__context, array($this->__id));
@@ -41,15 +48,24 @@ class ActiveRecord{
         }
     }
     
+    private function __return_active_field($field_name){
+        if ($this->__data->$field_name instanceof ActiveRecord) return $this->__data->$field_name;
+        $column = $this->__object->_columns[$field_name];
+        if($column instanceof Many2OneField){
+            $this->__data->$field_name = new ActiveRecord($column->relation_object, array($column->relation_object_key=>$this->__data->$field_name), $this->__context);
+        }
+        return $this->__data->$field_name;
+    }
+    
     public function __get($name){
         if (array_key_exists($name, $this->__params)) return $this->__params[$name];
         $obj = $this->__object;
         if (!array_key_exists($name, $obj->_columns)) throw new Exception('Column '.$name.' not found in '.$obj->_name);
         $this->__get_data();
         if (is_null($this->__data)) return $obj->_columns[$name]->get_default();
-        if (isset($this->__data->$name)) return $this->__data->$name;
+        if (isset($this->__data->$name)) return $this->__return_active_field($name);
         $this->__add_data($name);
-        return $this->__data->$name;
+        return $this->__return_active_field($name);
     }
 
     public function __set($name, $value){
