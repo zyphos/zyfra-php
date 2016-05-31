a1ad3b7a9492cd74b55921a464aaaedf3ee2f31f
Nicolas DS <zyphos@zyphos.be>
2016-05-31 14:11:10 +0200

    [IMP] ORM: allow translatable text field to be set at creation and at write

--- object/fields/texts.php
+++ object/fields/texts.php
@@ -2,20 +2,46 @@
 class TextField extends Field{
     var $widget='text';
     var $translate=false;
+    
+    public function is_stored(&$context){
+        return !$this->translate || !strlen(array_get($context, 'parameter'));
+    }
+    
+    protected function get_language_id(&$context){
+        $language_id = array_get($context, 'language_id');
+        $parameter = array_get($context, 'parameter');
+        $pool = &$this->object->_pool;
+        $object_tr = $pool->{$pool->get_language_object_name()};
+        if (is_numeric($parameter)){
+            $language_id = (int)$parameter;
+        }elseif($parameter){
+            $language_ids = $object_tr->name_search($parameter, $context);
+            if (count($language_ids) == 1) $language_id = $language_ids[0];
+        }
+        return $language_id;
+    }
 
     function sql_create($sql_create, $value, $fields, $context){
-        if (!$this->translate || !array_get($context, 'language_id')){
+        if (!$this->translate){
+            return parent::sql_create($sql_create, $value, $fields, $context);
+        }
+        
+        $language_id = $this->get_language_id($context);
+        
+        if (!$this->translate || !$language_id){
             return parent::sql_create($sql_create, $value, $fields, $context);
         }else{
-            $sql_create->add_callback($this, 'sql_create_after_trigger');
+            return new zyfra\orm\Callback('sql_create_after_trigger', null);
+            //$sql_create->add_callback($this, 'sql_create_after_trigger', $value, $fields, $context);
         }
         return null;
     }
 
-    function sql_create_after_trigger($sql_create, $value, $fields, $id, $context){
+    function sql_create_after_trigger($sql_create, $value, $fields, $context, $id){
+        $language_id = $this->get_language_id($context);
         $t = $this->translate;
         $tr_obj = $this->object->_pool->{$t['object']};
-        $create = array($t['column']=>$new_value, $t['key']=>$id, $t['language_id']=>$language_id);
+        $create = array($t['column']=>$value, $t['key']=>$id, $t['language_id']=>$language_id);
         $tr_obj->create($create, $context);
     }
 
@@ -24,21 +50,14 @@ class TextField extends Field{
         if (!$this->translate){
             return parent::sql_write($sql_write, $value, $fields, $context);
         }
-        $language_id = array_get($sql_write->context, 'language_id');
-        $parameter = array_get($context, 'parameter');
-        $t = &$this->translate;
-        $object_tr = $this->object->_pool->{$t['object']};
-        if (is_numeric($parameter)){
-            $language_id = (int)$parameter;
-        }elseif($parameter){
-            $language_ids = $object_tr->name_search($parameter);
-            if (count($language_ids) == 1) $language_id = $language_ids[0];
-        }
+        $language_id = $this->get_language_id($context);
         
         if (!$language_id){
-            parent::sql_write($sql_write, $value, $fields, $context);
-            return;
+            return parent::sql_write($sql_write, $value, $fields, $context);
         }
+        
+        $t = &$this->translate;
+        $object_tr = $this->object->_pool->{$t['object']};
         
         //'column'=>$name, 'key'=>'source_id', 'language_id'=>'language_id'
         $where = $t['key'].' IN %s AND '.$t['language_id'].'=%s';
@@ -88,6 +107,10 @@ class TextField extends Field{
                 $pool->{$tr_name}->add_column($name, $this->__get_translate_col_instance());
             }else{
                 $language_object_name = $pool->get_language_object_name();
+                try{
+                     $pool->{$language_object_name};
+                }catch (Exception $e) {}
+                
                 if (!$pool->object_in_pool($language_object_name)){
                     $lg_obj = new ObjectModel($pool, array(
                             '_name'=>$language_object_name,
