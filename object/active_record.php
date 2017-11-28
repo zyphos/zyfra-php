2ed3e9d176c67d0903ea49b28ebd4bba4c1d0065
Nicolas DS <zyphos@zyphos.be>
2017-11-28 12:03:59 +0100

    [IMP] ORM: active_record: add function exists + function prefetch fields

--- object/active_record.php
+++ object/active_record.php
@@ -7,6 +7,8 @@ class ActiveRecord{
     protected $__modified_columns;
     protected $__mql_where;
     protected $__id;
+    protected $__exists;
+    protected $__prefetch_fields;
 
     public function __construct($object, $params, $context = array()){
         /* $params is an array:
@@ -17,23 +19,41 @@ class ActiveRecord{
         $this->__object = $object;
         $this->__context = $context;
         $this->__data = null;
+        $this->__exists = null;
         if (!is_array($params)){
             $params = array($object->_key=>$params);
         }
         $this->__params = $params;
         $this->__modified_columns = array();
+        $key = $object->_key;
+        $this->__id = array_key_exists($key, $this->__params)?$this->__params[$key]:null;
+        $this->__mql_where = array_key_exists('mql_where', $this->__context)?$this->__context['mql_where']:$key.'=%s';
+        $this->__prefetch_fields = array_key_exists('mql_fields', $this->__context)?array_filter(explode(',',$this->__context['mql_fields'])):['*'];
     }
     
-    private function __get_data(){
+    public function prefetch_fields($fields=[]){
+        // Field array or string comma separated
+        
         // TODO: prefetch all stored field (like Many2one)
+        if (is_null($this->__id)) {
+            $this->__exists = False;
+            return;
+        }
+        if (is_string($fields)) $fields = array_filter(explode(',',$fields));
+        if (empty($fields) && (empty($this->__prefetch_fields) || !is_null($this->__data))) return;
+        
         $obj = $this->__object;
         $key = $obj->_key;
-        if(!is_null($this->__data) || (!array_key_exists($key, $this->__params) && !array_key_exists('mql_where', $this->__context))) return;
-        $this->__id = array_key_exists($key, $this->__params)?$this->__params[$key]:0;
-        $mql_fields = array_key_exists('mql_fields', $this->__context)?$this->__context['mql_fields']:'*';
-        $this->__mql_where = array_key_exists('mql_where', $this->__context)?$this->__context['mql_where']:$key.'=%s';
+        if (is_null($this->__data)) $fields = array_unique(array_merge($fields, $this->__prefetch_fields));
+        $mql_fields = implode(',', $fields);
         $result = $obj->select([$mql_fields.' WHERE '.$this->__mql_where, [$this->__id]], $this->__context);
-        if (count($result)) $this->__data = $result[0];
+        
+        if (count($result)) {
+            $this->__exists = true;
+            $this->__data = $result[0];
+        }else{
+            $this->__exists = False;
+        }
     }
     
     private function __add_data($mql_fields){
@@ -61,7 +81,7 @@ class ActiveRecord{
         if (array_key_exists($name, $this->__params)) return $this->__params[$name];
         $obj = $this->__object;
         if (!array_key_exists($name, $obj->_columns)) throw new Exception('Column '.$name.' not found in '.$obj->_name);
-        $this->__get_data();
+        $this->prefetch_fields([$name]);
         if (is_null($this->__data)) return $obj->_columns[$name]->get_default();
         if (isset($this->__data->$name)) return $this->__return_active_field($name);
         $this->__add_data($name);
@@ -84,19 +104,24 @@ class ActiveRecord{
             throw new Exception('Column '.$name.' not found in '.$this->__object->_name);
         }
     }
+    
+    public function exists(){
+        if (is_null($this->__exists)) $this->prefetch_fields([$this->__object->_key]);
+        return $this->__exists;
+    }
 
     public function save(){
         $obj = $this->__object;
         $key = $this->__object->_key;
         
-        if (array_key_exists($key, $this->__params)){
+        if ($this->exists()){
             $values = array();
             foreach($this->__modified_columns as $col_name=>$t){
                 $values[$col_name] = $this->__params[$col_name];
             }
             if (count($values) == 0) return; //Nothing to save
-            $obj->write($values, (int)$this->__params[$key], $this->__context);
-            return (int)$this->__params[$key];
+            $obj->write($values, (int)$this->__id, $this->__context);
+            return (int)$this->__id;
         }else{
             return $obj->create($this->__params, $this->__context);
         }
